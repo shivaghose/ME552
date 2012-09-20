45bdcaf3ee644c9c40ba833fc215086569101c7e
Shiva Ghose <shiva.ghose@gmail.com>
2012-09-20 18:29:33 -0400

    Updated Lab2(a) with figures and edits

---
+++ Lab2/lab2matlabstuffs/MagLevInit.m
@@ -0,0 +1,13 @@
+%OPA547 Params
+R1 = 46.2e3; %ohm
+R2 = 999; %ohm
+Rs = 0.5; %ohm
+
+%ElectroMechanical System Consatant
+C = 1; 
+
+%Sensor
+rblock = (6.35/1000); %m
+rbeam = (1.5048/1000); %m
+
+%ToDo: Create constants for the the computer-controller
