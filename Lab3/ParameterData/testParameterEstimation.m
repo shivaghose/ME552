00b346b65c6d460f2f66c32f3667214224cdc3be
John Peterson <jpeterson0913@gmail.com>
2012-10-10 13:07:14 -0400

    aded the most recent numbe

--- Lab3/ParameterData/testParameterEstimation.m
+++ Lab3/ParameterData/testParameterEstimation.m
@@ -8,9 +8,51 @@ Breverse = 6.728054e-4 * Kt; % 0.9*6.728054e-4 * Kt
 Tfforwards = 1.75*0.4637 * Kt; %1.75*0.4637 * Kt
 Tfreverse = 0.6071722 * Kt; % 
 
+% Bforwards = 0.000023725941605;
+% Breverse = 0.000020265029631;
+% Tfforwards = 0.034057546192782;
+% Tfreverse = 0.025282905174523;
+% 
+% J = 0.000043471112655;
+% Kt =  0.042000000000000;
+% Bforwards =  0.000002; % negative values are no good -0.000011421939988
+% Breverse = 0.000002;
+% Tfforwards = 0.033926022489524;
+% Tfreverse = 0.025288849161596;
+% 
+% J = 0.000046420828236;
+% Kt =    0.044062508073865;
+% Bforwards =    0.000001678546329;
+% Breverse =   0.000002112293049;
+% Tfforwards =   0.034540796108616;
+% Tfreverse =   0.026025786119668;
 
+% best so far with magnitude of error of 40.2317373
+%  0.000044142993118
+%   0.042016985993061
+%  -0.000018376326419
+%   0.000000158849427
+%   0.033878935596711
+%   0.025319881527519
 
-VAL = xlsread('shortSinusoidalInput.xls');
+% best numbers!!! magnitude of error of 27.649 on short data set with
+% 2*10^4 points
+J = 0.000035780751317;
+Kt = 0.032448801388174;
+Bforwards = 0.000032339885634;
+Breverse = 0.000041519670783;
+Tfforwards = 0.022294359068982;
+Tfreverse = 0.015632909278982;
+
+% error of 23.4941109 on short data set with 2*10^4 points
+% 0.000035051428588
+%    0.031449871029185
+%    0.000010858577896
+%    0.000048592997081
+%    0.022438919281204
+%    0.014309603079420
+
+VAL = xlsread('SinusoidalInput.xls');
 
 T = VAL(:,2); % delta Time
 Theta = VAL(:,3); % need negative
@@ -31,7 +73,7 @@ I = Voltage;
 %Pest = parameterSweep(T',Theta',I,P0)
 %Pest = nonuniformParameterEstimation(0,T,Theta,I,P0,);
 state0 = [J; Kt; Bforwards; Breverse; Tfforwards; Tfreverse];
-locked = [ 1; 1;     0    ;     0   ;     0    ;       0     ];
+locked = [ 0; 0;     0    ;     0   ;     0    ;       0     ];
 Pest = enchancedParameterEstimation(T,Theta,I,state0,locked)
 end
 
