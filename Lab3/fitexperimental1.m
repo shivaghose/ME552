c8bdf169c04cce619b17a8a72df87bfe0e4ac501
John Peterson <jpeterson0913@gmail.com>
2012-10-09 21:07:02 -0400

    I don't even know, this model is out of date

--- Lab3/fitexperimental1.m
+++ Lab3/fitexperimental1.m
@@ -2,7 +2,11 @@ function [Pest] = fitexperimental1()
 % uses parameterEstimation matlab function to fit the data
 
 %P0 = [8.5e-6; 3.7e-6; (5.6e-3)/2; 4.24e-2]; % initial guesses
-P0 = [0.0001; 0.0001; 0.1192; 0.0467] % better initial guess
+P0 = [0.0001; 0.0001; 0.1192; 0.0467]; % better initial guess
+P0 = [0.00015; 0.00010; 0.001; 0.0467];
+P0 = [0.000303362119919; 0.000202781054320; 0.0243853568; 0.09510344];
+P0 = [0.000060672423984; 0.000040556210864; 0.014631214080000; 0.019020688000000];
+P0 = [1e-5; 1e-5; 1e-4; 0.04];
 
 VAL = xlsread('ExperimentalData1.xls');
 
@@ -34,8 +38,8 @@ size(T')
 size(Theta')
 size(I)
 
-Pest = parameterSweep(T',Theta',I,P0)
-%Pest = parameterEstimation(0,T',Theta',I,P0)
+%Pest = parameterSweep(T',Theta',I,P0)
+Pest = parameterEstimation(0,T',Theta',I,P0)
 
 end
 
